f1fee225b4287d5acfced19faaf090313f5ab6a2
Pallab jyoti Saikia <53530099+pallab54@users.noreply.github.com>
2026-02-07 01:26:41 +0530

    Add files via upload

---
+++ loopBasic2.py
@@ -0,0 +1,11 @@
+i = 1
+while (i<=3):
+    i=i+1
+    print(i," pallab is a hacker")
+
+
+j = 4
+while (j>=1):
+    j=j-1
+    print(j,"you are haked")
+
---
+++ loosBasic.py
@@ -0,0 +1,5 @@
+num = 1
+while num <= 10:
+    print("pallab is a hacker")
+    num = num+1
+print(" you are hacked")
